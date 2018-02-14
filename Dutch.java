cd0c0962851b057745288763f0cb139c6bf477ef
David Gutierrez <david.a.gutierrez99@gmail.com>
2018-02-14 09:22:20 -0600

    Wednesday

--- Dutch.java
+++ Dutch.java
@@ -6,7 +6,7 @@ import java.awt.image.*;
 import java.io.*;
 import java.awt.event.*;
 
-public class Dutch extends JPanel implements MouseMotionListener{
+public class Dutch extends JPanel implements MouseMotionListener, MouseListener{
 	static final long serialVersionUID = 0L;
 	static String title = "Dutch Solitaire";
 	static String path = "res/";
@@ -19,13 +19,16 @@ public class Dutch extends JPanel implements MouseMotionListener{
 	static char[] values = {'A','2','3','4','5','6','7','8','9','T','J','Q','K'};
 	static Card[][] board = new Card[4][14];
 	static Color back = new Color(0, 153, 0);
+	static Card dragged;
 	public Dutch(){
 		try{
 			for(int i=0;i<suits.length;i++){
 				for(int j = 0;j<values.length;j++){
 					String key = String.valueOf(values[j]=='T'?10+"":values[j])+suits[i];
 					String p = path+key+".png";
-					board[i][j] = new Card(suits[i],values[j],ImageIO.read(new File(p)));
+					board[i][j] = new Card(suits[i],values[j],i,j,ImageIO.read(new File(p)));
+					board[i][j].x = j*w;
+					board[i][j].y = i*h;
 				}
 				swap(board, i, 0, i, board[i].length-1);
 			}
@@ -37,6 +40,8 @@ public class Dutch extends JPanel implements MouseMotionListener{
 		frame = new JFrame(title);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
+		frame.getContentPane().addMouseMotionListener(this);
+		frame.getContentPane().addMouseListener(this);
 		setPreferredSize(new Dimension(WIDTH,HEIGHT));
 		frame.add(this);
 		frame.getContentPane().setPreferredSize(new Dimension(WIDTH,HEIGHT));
@@ -53,62 +58,101 @@ public class Dutch extends JPanel implements MouseMotionListener{
 		int y = 0;
 		for(Card[] row:board){
 			for(Card card:row){
-				if(card!=null)
+				if(card!=null&&card==dragged)
+					g.drawImage(card.img, card.x, card.y, w, h, null);
+				else if(card!=null)
 					g.drawImage(card.img, x, y, w, h, null);
-					x+=w;
-					if(x+w>WIDTH){
-						x=0;
-						y+=h;
-					}
-				// }
+				x+=w;
+				if(x>WIDTH){
+					x=0;
+					y+=h;
+				}
 			}
 		}
 	}
 	public static void main(String[] args) {
-		while(true){
-			ProcessBuilder p1 = new ProcessBuilder("C:/Program Files (x86)/Google/Chrome/Application/chrome","http://www.dafk.net/what/");
-			Process p = p1.start();
-			try{
-				Thread.sleep(50);
-			}catch(Exception e){}
+		new Dutch();
+	}
+	public void mouseReleased(MouseEvent e){
+		int x = e.getX();
+		int y = e.getY();
+		int r = x/w;
+		int c = y/h;
+		debug(4);
+		if(dragged!=null){
+			swap(board, r, c, dragged.r, dragged.c);
+			debug(3);
 		}
-		//new Dutch();
+	}
+	public void mouseExited(MouseEvent e){
+		
+	}
+	public void mouseEntered(MouseEvent e){
+	
+	}
+	public void mousePressed(MouseEvent e){
+	
+	}
+	public void mouseClicked(MouseEvent e){
+	
 	}
 	public void mouseMoved(MouseEvent e){
 	
 	}
 	public void mouseDragged(MouseEvent e){
-	
+		int x = e.getX();
+		int y = e.getY();
+		if(dragged!=null){
+			dragged.x = x;
+			dragged.y = y;
+		}else{
+			int r = x/w;
+			int c = y/h;
+			if(r<board.length&&c<board[0].length-2&&c>=0&&r>=0){
+				debug(1);
+				dragged = board[r][c];
+			}
+		}
 	}
-	public void swap(Object[][] arr, int ri, int re, int ci, int ce, int times){
+	public void swap(Card[][] arr, int ri, int re, int ci, int ce, int times){
 		for(int i=0;i<times;i++){
 			int r = ri+(int)(Math.random()*(re-ri));
 			int c = ci+(int)(Math.random()*(ce-ci));
 			int r2 = ri+(int)(Math.random()*(re-ri));
 			int c2 = ci+(int)(Math.random()*(ce-ci));
-			debug(r,c,r2,c2);
-			Object temp = arr[r][c];
+			// debug(r,c,r2,c2);
+			Card temp = arr[r][c];
 			arr[r][c] = arr[r2][c2];
 			arr[r2][c2] = temp;
 		}
 	}
-	public void swap(Object[][] arr, int ri, int ci, int re, int ce){
-		Object temp = arr[ri][ci];
+	public void swap(Card[][] arr, int ri, int ci, int re, int ce){
+		Card temp = arr[ri][ci];
+		if(temp!=null){
+			temp.r = re;
+			temp.c = ce;
+		}
 		arr[ri][ci] = arr[re][ce];
+		if(arr[ri][ci]!=null){
+			arr[ri][ci].r = ri;
+			arr[ri][ci].c = ci;
+		}
 		arr[re][ce] = temp;
 	}
 }
 class Card{
 	BufferedImage img;
-	// int x;
-	// int y;
+	int r;
+	int c;
+	int x;
+	int y;
 	char suit;
 	char val;
-	public Card(char s, char v, BufferedImage i){
+	public Card(char s, char v, int x1, int y1, BufferedImage i){
 		suit = s;
 		val = v;
 		img = i;
-		// x = x1;
-		// y = y1;
+		r = x1;
+		c = y1;
 	}
 }
