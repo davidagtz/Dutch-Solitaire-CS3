35b4ec6edbd826fd19b4fa224a0cefafa898e1db
David Gutierrez <david.a.gutierrez99@gmail.com>
2018-02-15 09:48:56 -0600

    Rules Implemented

--- Dutch.java
+++ Dutch.java
@@ -13,13 +13,16 @@ public class Dutch extends JPanel implements MouseMotionListener, MouseListener{
 	static JFrame frame;
 	static int h = 100;
 	static int w = h*2/3; 
+	static Font font = new Font(Font.SANS_SERIF, Font.BOLD, h);
 	static final int WIDTH = w*14;
 	static final int HEIGHT = h*4;
 	static char[] suits = {'D','C','H','S'};
 	static char[] values = {'A','2','3','4','5','6','7','8','9','T','J','Q','K'};
-	static Card[][] board = new Card[4][14];
+	static Card[][] board = new Card[suits.length][values.length+1];
+	static String[][] comp = new String[suits.length][values.length+1];
 	static Color back = new Color(0, 153, 0);
 	static Card dragged;
+	static boolean won = false;
 	public Dutch(){
 		try{
 			for(int i=0;i<suits.length;i++){
@@ -29,6 +32,7 @@ public class Dutch extends JPanel implements MouseMotionListener, MouseListener{
 					board[i][j] = new Card(suits[i],values[j],i,j,ImageIO.read(new File(p)));
 					board[i][j].x = j*w;
 					board[i][j].y = i*h;
+					comp[i][j] = key;
 				}
 				swap(board, i, 0, i, board[i].length-1);
 			}
@@ -54,20 +58,43 @@ public class Dutch extends JPanel implements MouseMotionListener, MouseListener{
 	public void paintComponent(Graphics g){
 		g.setColor(back);
 		g.fillRect(0,0,WIDTH,HEIGHT);
-		int x = 0;
-		int y = 0;
-		for(Card[] row:board){
-			for(Card card:row){
-				if(card!=null&&card==dragged)
-					g.drawImage(card.img, card.x, card.y, w, h, null);
-				else if(card!=null)
-					g.drawImage(card.img, x, y, w, h, null);
-				x+=w;
-				if(x>WIDTH){
-					x=0;
-					y+=h;
+		if(won){
+			g.setFont(font);
+			int wi = g.getFontMetrics().stringWidth("YOU WON");
+			g.drawString("YOU WON", WIDTH/2-wi/2, HEIGHT/2-h/2);
+		}else{
+			int x = 0;
+			int y = 0;
+			boolean hasWon = true;
+			for(Card[] row:board){
+				for(Card card:row){
+					if(card!=null&&!card.equals(dragged))
+						g.drawImage(card.img, card.c*w, card.r*h, w, h, null);
+					x+=w;
+					if(x>WIDTH){
+						x=0;
+						y+=h;
+					}
 				}
 			}
+			if(null!=dragged){
+				g.drawImage(dragged.img, dragged.x, dragged.y, w, h, null);
+			}
+			// search:
+			// for(int i = 0;i<board.length;i++){
+			// 	for(int j = 0;j<board[i].length;i++){
+			// 		debug(comp[i][j]);
+			// 		debug(board[i][j]);
+			// 		if(comp[i][j]==null^board[i][j]==null){
+			// 			hasWon=false;
+			// 			break search;
+			// 		}
+			// 		else if(comp!=null&&!comp[i][j].equals(""+board[i][j].val+board[i][j].suit)){
+			// 			hasWon=false;
+			// 			break search;
+			// 		}
+			// 	}
+			// }
 		}
 	}
 	public static void main(String[] args) {
@@ -76,13 +103,18 @@ public class Dutch extends JPanel implements MouseMotionListener, MouseListener{
 	public void mouseReleased(MouseEvent e){
 		int x = e.getX();
 		int y = e.getY();
-		int r = x/w;
-		int c = y/h;
-		debug(4);
-		if(dragged!=null){
-			swap(board, r, c, dragged.r, dragged.c);
-			debug(3);
+		int c = x/w;
+		int r = y/h;
+		if(dragged!=null&&c<board[0].length-1&&board[r][c]==null){
+			if(dragged.val=='2'){
+				if(board[dragged.r][0]==null)
+					swap(board, r, c, dragged.r, dragged.c);
+			}
+			else
+					swap(board, r, c, dragged.r, dragged.c);
 		}
+		dragged = null;
+		repaint();
 	}
 	public void mouseExited(MouseEvent e){
 		
@@ -106,13 +138,13 @@ public class Dutch extends JPanel implements MouseMotionListener, MouseListener{
 			dragged.x = x;
 			dragged.y = y;
 		}else{
-			int r = x/w;
-			int c = y/h;
-			if(r<board.length&&c<board[0].length-2&&c>=0&&r>=0){
-				debug(1);
+			int c = x/w;
+			int r = y/h;
+			if(r<board.length&&c<board[0].length-1&&c>=0&&r>=0){
 				dragged = board[r][c];
 			}
 		}
+		repaint();
 	}
 	public void swap(Card[][] arr, int ri, int re, int ci, int ce, int times){
 		for(int i=0;i<times;i++){
@@ -121,9 +153,10 @@ public class Dutch extends JPanel implements MouseMotionListener, MouseListener{
 			int r2 = ri+(int)(Math.random()*(re-ri));
 			int c2 = ci+(int)(Math.random()*(ce-ci));
 			// debug(r,c,r2,c2);
-			Card temp = arr[r][c];
-			arr[r][c] = arr[r2][c2];
-			arr[r2][c2] = temp;
+			swap(arr, r, c, r2, c2);
+			// Card temp = arr[r][c];
+			// arr[r][c] = arr[r2][c2];
+			// arr[r2][c2] = temp;
 		}
 	}
 	public void swap(Card[][] arr, int ri, int ci, int re, int ce){
@@ -154,5 +187,11 @@ class Card{
 		img = i;
 		r = x1;
 		c = y1;
+	}
+	public boolean equals(Card card){
+		return card!=null&&r==card.r&&c==card.c;
+	}
+	public String toString(){
+		return ""+val+suit;
 	}
 }
